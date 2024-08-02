3f8cd572418933e7556f0f89b791a7241554c92b
Amila Indika <amilaindika789@gmail.com>
2024-08-01 17:40:53 -1000

    Extract main logic to a main function

--- src/pkg/caeser_cipher.py
+++ src/pkg/caeser_cipher.py
@@ -1,9 +1,10 @@
 import string
-from caeser_cipher_ascii_art import caeser_cipher_logo
+from src.pkg.caeser_cipher_ascii_art import caeser_cipher_logo
 
 print(caeser_cipher_logo)
 
 lowercase_alphabet = list(string.ascii_lowercase)
+
 
 def find_encrypt_letter_index(letter_index, shift_amount):
     new_index = (letter_index + shift_amount) % 26
@@ -11,8 +12,9 @@ def find_encrypt_letter_index(letter_index, shift_amount):
 
 
 def find_decrypt_letter_index(letter_index, shift_amount):
-    new_index = (letter_index - shift_amount)
+    new_index = letter_index - shift_amount
     return new_index
+
 
 def caeser(operation, input_text, alphabet, shift_amount):
     output_text = ""
@@ -30,18 +32,33 @@ def caeser(operation, input_text, alphabet, shift_amount):
         else:
             output_text += character
 
-    return output_text  
+    return output_text
 
-is_rerunning = True
-while is_rerunning:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
 
-    shift = shift % 26
-    print(caeser(direction, text, lowercase_alphabet, shift))
+def get_user_input(prompt):
+    return input(prompt)
 
-    rerun = input("Type 'yes' if you want to go again. Otherwise 'no'.\n").lower()
-    if rerun == "no":
-        is_rerunning = False
-        print("Good Bye!")
+
+def main():
+    encrypt_decrypt_prompt = "Type 'encode' to encrypt, \
+    \nType 'decode' to decrypt:\n"
+
+    is_rerunning = True
+    while is_rerunning:
+        direction = get_user_input(encrypt_decrypt_prompt)
+        text = get_user_input("Type your message:\n").lower()
+        shift = int(get_user_input("Type the shift number:\n"))
+
+        shift = shift % 26
+        print(caeser(direction, text, lowercase_alphabet, shift))
+
+        rerun = get_user_input(
+            "Type 'yes' if you want to go again. Otherwise 'no'.\n"
+        ).lower()
+        if rerun == "no":
+            is_rerunning = False
+            print("Good Bye!")
+
+
+if __name__ == "__main__":
+    main()
