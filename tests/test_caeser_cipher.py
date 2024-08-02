e8abbe6f63f567228228ac3759e59099fa3e3526
Amila Indika <amilaindika789@gmail.com>
2024-08-01 17:41:10 -1000

    Add unit test for find_encrypt_letter_index function

--- tests/test_caeser_cipher.py
+++ tests/test_caeser_cipher.py
@@ -2,7 +2,13 @@ import sys
 
 sys.path.append("../src")
 
-# from src.pkg.caeser_cipher import *
+from src.pkg.caeser_cipher import *
 
-def test_dummy():
-    pass
+def test_find_encrypt_letter_index_with_shift_less_than_26():
+    assert find_encrypt_letter_index(3, 5) == 8
+    assert find_encrypt_letter_index(5, 8) == 13
+
+
+def test_find_encrypt_letter_index_with_shift_greater_than_26():
+    assert find_encrypt_letter_index(3, 50) == 1
+    assert find_encrypt_letter_index(9, 100) == 5
